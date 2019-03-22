d73d6e3fef37ec7f7409daf64defc70c17b153a2
Tom Michiels <tmichiels@gmail.com>
2019-03-22 10:12:41 +0000

    merged disparate branches

---
+++ Practice/src/switches/Parking.java
@@ -0,0 +1,18 @@
+package switches;
+
+import java.util.Scanner;
+
+public class Parking {
+
+    Scanner myScanner = new Scanner(System.in);
+
+    private void getInput() {
+
+    }
+
+    public static void main(String[] args) {
+        Parking ghParking = new Parking();
+        ghParking.getInput();
+    }
+
+}
